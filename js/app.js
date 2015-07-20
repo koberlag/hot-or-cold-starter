5683b93892bd834f8545748f27abd5a0d98227f6
koberlag <koberlag@gmail.com>
2015-07-19 19:58:39 -0500

    added absolute feedback functionlity. Added functionality to setup a new game.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+var secretNumber=0;
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -12,6 +12,57 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	generateNumber();
+  	$(".new").click(newGame);
+
+  	$("form").submit(function(e){
+  		getFeedback();
+  		return false
+  	});
 });
+
+function newGame(){
+	$("#feedback").text("Make your Guess!");
+	$("#count").text("0");
+	$("#userGuess").val("");
+	generateNumber();
+}
+
+function generateNumber(){
+	var max = 100, min = 1;
+	secretNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getFeedback(){
+	var difference = Math.abs(secretNumber - $("#userGuess").val()),
+		feedback   = $("#feedback");
+
+	if(difference >= 50){
+		feedback.text("Ice Cold");
+	}
+	else if(difference >= 30){
+		feedback.text("Cold");
+
+	}
+	else if(difference >=20){
+		feedback.text("Warm");
+	}
+	else if(difference >= 10){
+		feedback.text("Hot");
+	}
+	else if(difference >= 1){
+		feedback.text("Very Hot");
+	}
+	else if(difference == 0){
+		feedback.text("Winner!");
+	}
+
+	increaseCount();
+}
+
+function increaseCount(){
+	var countContainer= $("#count");
+	countContainer.text(+countContainer.text() + 1);
+}
 
 
